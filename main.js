b496d30608083847c4e5e2e880b33af3837b4c67
Mathieuleysen <mathieu.leysen@gmail.com>
2017-02-06 15:32:19 +0100

    edit main

--- main.js
+++ main.js
@@ -98,6 +98,8 @@ function main() {
                
            }
        }
+
+       $('.parallax-window').parallax({imageSrc: 'img/IMG_3249_blur.jpg'});
    }
     
     
@@ -132,14 +134,6 @@ function main() {
     scrollTo(dest.offsetTop, 500);
 }
     ///////////////////////////////////
-    
-    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-  ga('create', 'UA-40231338-2', 'auto');
-  ga('send', 'pageview');
 }
 
 window.onload = function() {
